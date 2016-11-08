8130b18b142ef5cc4047a0ea93d237121e468c88
marchelo1989 <ma.burgosm@alumnos.duoc.cl>
2016-11-08 04:00:19 -0300

    prueba3

--- app/src/main/java/com/example/marcelo/inventario/DBHelper.java
+++ app/src/main/java/com/example/marcelo/inventario/DBHelper.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by marcelo on 03-11-2016.
- */
 public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -14,13 +11,13 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table usuarios(id integer primary key autoincrement,nombre text,usuario text,contrasena text)");
+        db.execSQL("create table usuarios(nombre text,usuario text,contrasena text)");
         db.execSQL("insert into usuarios values('admin','admin','admin')");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("create table usuarios(codigo integer primary key,usuario text,contrasena text)");
-        db.execSQL("insert into usuarios values(01,'admin','admin')");
+        db.execSQL("create table usuarios(nombre text,usuario text,contrasena text)");
+        db.execSQL("insert into usuarios values('admin','admin','admin')");
     }
 }
